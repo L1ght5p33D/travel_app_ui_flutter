03e88a69e22f0f3689b189a2d19b5169c4b63231
ls <ryanadrig@gmail.com>
2023-01-27 20:00:27 -0800

    dest carousel style progress

--- lib/destinationCaro.dart
+++ lib/destinationCaro.dart
@@ -1,22 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:travel_places_ui/destinationDetail.dart';
 
 List<Map> destinationdata = [
   {
     "destinationName": "Paris",
     "activityCount": "3 Activites",
-    "description": "Stuff",
+    "description":
+        "Paris is a city unlike any other. It is overflowing with culture, history, and beauty. And while people travel to Paris to see the Louvre",
     "img_path": "assets/images/paris_travel.webp"
   },
   {
     "destinationName": "New York",
     "activityCount": "3 Activites",
-    "description": "ny stuff",
+    "description":
+        "Cool, cosmopolitan, crowded, constantly evolving … the Big Apple blends big city splendor with small-town charm. Amid New York's iconic landmarks and towering skyscrapers",
     "img_path": "assets/images/paris_travel.webp"
   },
   {
     "destinationName": "Tokyo",
     "activityCount": "3 Activites",
-    "description": " tokyo stuff",
+    "description":
+        "Today, Tokyo offers a seemingly unlimited choice of shopping, entertainment, culture and dining to its visitors. The city's history can be",
     "img_path": "assets/images/paris_travel.webp"
   }
 ];
@@ -62,17 +66,23 @@ class _DestinationCarouselState extends State<DestinationCarousel> {
                       padding: EdgeInsets.fromLTRB(
                           ss.width * .04, ss.width * .02, ss.width * .04, 0.0),
                       child: GestureDetector(
-                        onTap: () {},
+                        onTap: () {
+                          Navigator.of(context)
+                              .push(MaterialPageRoute(builder: (context) {
+                            return DestinationDetail(
+                                destinationdata[destinations_idx]);
+                          }));
+                        },
                         child: Container(
-                            width: ss.width * .35,
+                            width: ss.width * .46,
                             child: Stack(
                               alignment: Alignment.topCenter,
                               children: [
                                 Positioned(
                                     bottom: ss.height * .01,
                                     child: Container(
-                                        height: ss.height * .15,
-                                        width: ss.width * .36,
+                                        height: ss.height * .47,
+                                        width: ss.width * .46,
                                         decoration: BoxDecoration(
                                             color:
                                                 Theme.of(context).canvasColor,
@@ -96,23 +106,26 @@ class _DestinationCarouselState extends State<DestinationCarousel> {
                                           ],
                                         ))),
                                 Container(
-                                  height: ss.height * .25,
+                                  height: ss.width * .46,
                                   child: Stack(children: [
                                     Hero(
-                                        tag: destinationdata[destinations_idx]
-                                            ["destinationName"],
-                                        child: Container(
-                                            height: ss.height * .25,
-                                            width: ss.height * .25,
-                                            decoration: BoxDecoration(
-                                                borderRadius:
-                                                    BorderRadius.circular(
-                                                        ss.width * .02)),
-                                            child: Image.asset(
-                                                destinationdata[
-                                                        destinations_idx]
-                                                    ["img_path"],
-                                                fit: BoxFit.cover))),
+                                      tag: destinationdata[destinations_idx]
+                                          ["destinationName"],
+                                      child: Container(
+                                          height: ss.width * .46,
+                                          width: ss.width * .46,
+                                          child: ClipRRect(
+                                              borderRadius:
+                                                  BorderRadius.circular(
+                                                      ss.width * .04),
+                                              child: Image.asset(
+                                                  destinationdata[
+                                                          destinations_idx]
+                                                      ["img_path"],
+                                                  height: ss.height * .25,
+                                                  width: ss.height * .25,
+                                                  fit: BoxFit.fill))),
+                                    ),
                                     Container(
                                         height: ss.height * .25,
                                         child: Column(
@@ -124,7 +137,9 @@ class _DestinationCarouselState extends State<DestinationCarousel> {
                                                         destinations_idx]
                                                     ["destinationName"],
                                                 style: TextStyle(
-                                                    color: Colors.white))
+                                                    color: Colors.white,
+                                                    fontWeight: FontWeight.w900,
+                                                    fontSize: ss.width * .06))
                                           ],
                                         ))
                                   ]),
